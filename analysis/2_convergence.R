e64ba4692b4e7d7d981d2eaec8a7ce7e425a263e
ballardtj <ballardtj@gmail.com>
2019-10-15 08:48:25 +1000

    Run models with gamma distributed parameters

--- analysis/2_convergence.R
+++ analysis/2_convergence.R
@@ -13,17 +13,21 @@ library(rstan)
 library(coda)
 library(knitr)
 
-# library(shinystan)
-# 
-# launch_shinystan(fit)
 
-source("models/model_details.R")
+library(shinystan)
+# 
+launch_shinystan(fit)
+
+
 
 stan2coda <- function(fit) {
   mcmc.list(lapply(1:ncol(fit), function(x) mcmc(as.array(fit)[,x,])))
 }
 
-for( exp in 1){
+for( exp in 1:2){
+  
+source("models/model_details.R")
+  
 for (i in 1:length(models)){
 
   if(i != 4 & i != 6){names(models)[i] = paste0(names(models)[i],"_gamma")}
@@ -38,21 +42,21 @@ for (i in 1:length(models)){
 
   write.csv(smrystan[[1]], file=paste0("figures/summary_exp",exp,"_",names(models)[i],".csv"))
   
-  #grep(models[[i]]$parms,rownames(smrystan[[1]]))
-  
-  smrystan[[1]][grep(models[[i]]$parms,rownames(smrystan[[1]])),]
-
-  print(kable(effectiveSize(stan2coda(fit))[models[[i]]$hypers],format="markdown",col.names="n_eff"))
-
-  try(print(kable(gelman.diag(stan2coda(fit))[[1]][models[[i]]$hypers,],format="markdown")))
-
-  traceplot = rstan::traceplot(fit,par=models[[i]]$hypers)
-  ggsave(file=paste0("figures/convergence_trace_exp",exp,"_",names(models)[i],".pdf"),plot=traceplot)
-
-
-  pdf(file=paste0("figures/convergence_pairs_exp",exp,"_",names(models)[i],".pdf"),width=10,height=10)
-     pairs(fit,pars=models[[i]]$hypers)
-  dev.off()
+  # #grep(models[[i]]$parms,rownames(smrystan[[1]]))
+  # 
+  # smrystan[[1]][grep(models[[i]]$parms,rownames(smrystan[[1]])),]
+  # 
+  print(kable(effectiveSize(stan2coda(fit)),format="markdown",col.names="n_eff"))
+  # 
+  # try(print(kable(gelman.diag(stan2coda(fit))[[1]][models[[i]]$hypers,],format="markdown")))
+  # 
+  # traceplot = rstan::traceplot(fit,par=models[[i]]$hypers)
+  # ggsave(file=paste0("figures/convergence_trace_exp",exp,"_",names(models)[i],".pdf"),plot=traceplot)
+  # 
+  # 
+  # pdf(file=paste0("figures/convergence_pairs_exp",exp,"_",names(models)[i],".pdf"),width=10,height=10)
+  #    pairs(fit,pars=models[[i]]$hypers)
+  # dev.off()
   
 }
 }
